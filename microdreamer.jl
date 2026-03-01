00a9b46e5226798f8d3c38cfa82acb3423ece966
ssrhaso <hasaana2005@gmail.com>
2026-03-01 09:46:39 +0000

    INIT: Comments

--- microdreamer.jl
+++ microdreamer.jl
@@ -1,1 +1,6 @@
-println("Setup", VERSION)
+"""
+The most atomic way to train and run inference for a World Model in pure, dependency-free Julia.
+This file is the complete algorithm: encoder -> cross-attention world state -> decoder.
+Everything else is just efficiency. Inspired by microgpt @karpathy
+@ssrhaso
+"""
